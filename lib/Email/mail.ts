ccbdcfa46e533cece9d8b8f22fc6d3de4ea60349
MOHITH-2002 <mohithba9353@gmail.com>
2024-05-05 11:41:25 +0530

    Email sending ui changed

--- lib/Email/mail.ts
+++ lib/Email/mail.ts
@@ -14,7 +14,16 @@ export const sendVerificationEmail = async (
     from: "mail@qrtickets.software",
     to: email,
     subject: "Confirm your email",
-    html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
+    html: `# Final step...
+
+Follow this link to verify your email address.
+
+<a href="${confirmLink} target="_blank" style="padding: 12px 24px; border-radius: 4px; color: #FFF; background: #2B52F5;display: inline-block;margin: 0.5rem 0;">Confirm now</a>
+
+If you didn't ask to verify this address, you can ignore this email.
+
+Thanks,
+Team Qr-Tickets`
   });
 };
 
@@ -29,6 +38,27 @@ export const sendPasswordResetEmail = async (
     from: "mail@qrtickets.software",
     to: email,
     subject: "Reset your password",
-    html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
+    html: `# Trouble signing in?
+
+We've received a request to reset the password for this user account.
+
+<a href="${resetLink}" target="_blank" style="padding: 12px 24px; border-radius: 4px; color: #FFF; background: #2B52F5;display: inline-block;margin: 0.5rem 0;">Reset your password</a>
+
+If you didn't ask to reset your password, you can ignore this email.
+
+Thanks,
+Team Qr-Tickets`
   });
 };
+
+// export const sendTicketToEmail = async (
+//   email: string,
+//   token: string,
+// ) => {
+//   await resend.emails.send({
+//     from: "mail@qrtickets.software",
+//     to: email,
+//     subject: "Reset your password",
+    
+//   });
+// };
