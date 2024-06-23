fa9e5affb214a3f19023a03daaa8cab2df684499
MOHITH-2002 <mohithba9353@gmail.com>
2024-06-24 01:20:18 +0530

    redirct fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,24 +1,27 @@
-"use client"
+"use client";
 
+import { useRouter } from 'next/navigation';
 import { Button } from "@/components/ui/button";
 import { ArrowLeftFromLine } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
 
-const Error = ()=>{
-    return(
-        <div className="h-full flex flex-col items-center justify-center space-y-5 ">
-            <Image src="https://res.cloudinary.com/dpgxmmowq/image/upload/v1708458521/404error_s32xro.svg" height={290} width={500}  alt="error" />
+const Error = () => {
+    const router = useRouter();
+
+    const handleRedirect = () => {
+        router.push('/');
+    };
+
+    return (
+        <div className="h-full flex flex-col items-center justify-center space-y-5">
+            <Image src="https://res.cloudinary.com/dpgxmmowq/image/upload/v1708458521/404error_s32xro.svg" height={290} width={500} alt="error" />
         
-            <Link href="/">
-            <Button>
+            <Button onClick={handleRedirect}>
                 <ArrowLeftFromLine />
-                
-                Go back</Button>
-            </Link>
-
-
+                Go back
+            </Button>
         </div>
     );
-}
+};
+
 export default Error;
